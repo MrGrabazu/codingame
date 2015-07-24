5dfc87ad88ecdf708c5e84ca15a6d30c3ed6f86d
MrGrabazu <kprudhomm@gmail.com>
2015-07-24 10:47:35 +0200

    new levels.

---
+++ Level0007.ASCIIArt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+        Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+        for (int i = 0; i < height; i++)
+        {
+            string ROW = Console.ReadLine();
+            foreach(char letter in alphabet)
+            {
+                string key = letter.ToString().ToLower();
+                string line = ROW.Substring(0, length);
+                ROW = ROW.Substring(length);
+                if (map.ContainsKey(key) == false)
+                    map.Add(key, new List<string>() { line });
+                else
+                    map[key].Add(line);
+            }
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        for (int i = 0; i < height; i++)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char letter in input)
+            {
+                string key = letter.ToString().ToLower();
+                if (alphabet.IndexOf(key, StringComparison.OrdinalIgnoreCase) == -1)
+                    key = "?";
+                builder.Append(map[key][i]);
+            }
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ Level0008.ChuckNorris/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine(ToUnary(ToBinary(Encoding.ASCII.GetBytes(input))));
+    }
+
+    public static string ToBinary(byte[] data)
+    {
+        return String.Join("", data.Select(x => Convert.ToString(x, 2).PadLeft(7, '0')));
+    }
+
+    public static string ToUnary(string bits)
+    {
+        List<Tuple<char, string>> groups = new List<Tuple<char, string>>();
+        char current = bits.FirstOrDefault();
+        StringBuilder builder = new StringBuilder();
+        foreach(char bit in bits)
+        {
+            if (bit != current && builder.Length > 0)
+            {
+                groups.Add(new Tuple<char, string>(current, builder.ToString()));
+                current = bit;
+                builder = new StringBuilder();
+            }
+            builder.Append(bit);
+        }
+        groups.Add(new Tuple<char, string>(current, builder.ToString()));
+
+        List<string> result = new List<string>();
+        foreach(Tuple<char, string> group in groups)
+        {
+            string s = "";
+            int l = 0;
+            if (group.Item1 == '0')
+            {
+                s = "00 ";
+                l = 3;
+            }
+            else
+            {
+                s = "0 ";
+                l = 2;
+            }
+
+            s = s.PadRight(group.Item2.Length + l, '0');
+            result.Add(s);
+        }
+
+        return String.Join(" ", result);
+    }
+}
